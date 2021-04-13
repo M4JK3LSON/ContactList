4758a769f2afaa84b0a9d2c19a6653117a95ddb2
Michal L <Michal@imac-programista-2.local>
2021-04-13 12:02:37 +0200

    Initial Commit

---
+++ contactList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  contactList
+//
+//  Created by Michal L on 13.04.2021.
+//  Copyright © 2021 Michal L. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
